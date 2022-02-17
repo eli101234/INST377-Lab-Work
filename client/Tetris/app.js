9744eb4dd19fc700a7c3c1e7917e0fc2c9f0fb0e
Eli Hernandez <elih717@gmail.com>
2022-02-17 01:00:25 -0500

    Added all tetraminoes
    oh god, why

--- client/Tetris/app.js
+++ client/Tetris/app.js
@@ -1,3 +1,61 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  const squares = Array.from(document.querySelectorAll('.grid div'));
+  const ScoreDisplay = document.querySelector('#score');
+  const StartBtn = document.querySelector('#start-button');
+  const width = 10;
+  // lTetrominoes
+  const lTetrominoes = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width,width * 2, width * 2 + 1, width * 2 + 2]
+  ];
+  // zTetrominoes
+  const zTetrominoes = [
+    [0, width, width + 1, width * 2 + 1],
+    [width * 2, width + 1, width * 2 + 1, width + 2],
+    [0, width, width + 1, width * 2 + 1],
+    [width * 2, width + 1, width * 2 + 1, width + 2]
+  ];
+  // tSpin
+  const tTetrominoes = [
+    [width, 1, width + 1, width + 2],
+    [1, width + 1, width * 2 + 1, width + 2],
+    [width, width + 1, width * 2 + 2, width + 2],
+    [width, 1, width + 1, width * 2 + 1]
+  ];
+  // O
+  const oTetrominoes = [
+    [0, width, 1, width + 1],
+    [0, width, 1, width + 1],
+    [0, width, 1, width + 1],
+    [0, width, 1, width + 1]
+  ];
+  // i of the gods
+  const iTetrominoes = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+  ];
+  const theTetrominoes = [lTetrominoes, zTetrominoes, tTetrominoes, oTetrominoes, iTetrominoes];
+  let currentPosition = 4;
+  let currentRotation = 0;
+  // randomly select
+  let random = Math.floor(Math.random() * theTetrominoes.length);
+  let current = theTetrominoes[random][currentRotation];
+  // draw the first rotation in the first tetromino
+  function draw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('tetromino');
+    });
+  }
 
+  // undraw the Tetra
+  function undraw() {
+      current.forEach(index => {
+          squares[currentPosition + index].classList.remove('tetromino')
+      })
+  }
 });
