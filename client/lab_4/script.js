a425f1ac280785a0146458f185f6d8246c51b0e9
Eli Hernandez <elih717@gmail.com>
2022-02-19 22:29:06 -0500

    Did all of lab 4, basically
    Ended up doing all of lab 4 without early commits (sorry!) but I got it to work. Looks a little different than the video shown on the assignment, but it is roughly the same

---
+++ client/lab_4/script.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let slidePosition = 0;
+  const slides = document.querySelectorAll('.carousel_item');
+  const totalSlides = slides.length;
+
+  document
+    .querySelector('#nextBtn')
+    .addEventListener('click', () => {
+      moveToNextSlide();
+    });
+  document
+    .querySelector('#prevBtn')
+    .addEventListener('click', () => {
+      moveToPrevSlide();
+    });
+
+  function updateSlidePosition() {
+    for (const slide of slides) {
+      slide.classList.remove('carousel_item--visible');
+      slide.classList.add('carousel_item--hidden');
+    }
+
+    slides[slidePosition].classList.add('carousel_item--visible');
+  }
+
+  function moveToNextSlide() {
+    if (slidePosition === totalSlides - 1) {
+      slidePosition = 0;
+    } else {
+      slidePosition++;
+    }
+
+    updateSlidePosition();
+  }
+
+  function moveToPrevSlide() {
+    if (slidePosition === 0) {
+      slidePosition = totalSlides - 1;
+    } else {
+      slidePosition--;
+    }
+
+    updateSlidePosition();
+  }
+});
