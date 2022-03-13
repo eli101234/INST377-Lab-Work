4b2abf932af44db3c4dd9b3bd74731f5b2e77a41
Eli Hernandez <elih717@gmail.com>
2022-03-12 21:55:11 -0500

    Practically finished all of it
    Added createHtmlList, reworked the link, got rid of a bunch of log stuff, renamed one of the functions to restoArrayMake

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -6,30 +6,41 @@ function getRandomIntInclusive(min, max) {
   );
 }
 
-function dataHandler(dataArray) {
-  console.log('fired dataHandler');
-  console.table(dataArray);
+function restoArrayMake(dataArray) {
   const range = [...Array(15).keys()];
-  range.forEach((item) => {
-    console.log('rangeitem', item);
+  const listItems = range.map((item, index) => {
+    const restNum = getRandomIntInclusive(0, dataArray.length - 1);
+    return dataArray[restNum];
+  });
+  return listItems;
+}
+
+function createHtmlList(collection) {
+  const targetList = document.querySelector('.resto-list');
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const {name} = item;
+    const displayName = name.toLowerCase();
+    const injectThisItem = `<li>${displayName}</li>`;
+    targetList.innerHTML += injectThisItem;
   });
 }
+
 async function mainEvent() {
-  console.log('script loaded');
   const form = document.querySelector('.left-box');
   const submit = document.querySelector('.submit-button');
   submit.style.display = 'none';
 
-  const results = await fetch('/api/foodServicesPG');
+  const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
   const arrayFromJson = await results.json();
-  if (arrayFromJson.data.length > 0) {
+  if (arrayFromJson.length > 0) {
+    submit.style.display = 'block';
     form.addEventListener('submit', async (submitEvent) => {
       submitEvent.preventDefault();
-      console.log('form submission');
-      button.style.disply = 'block';
+      const restoArray = restoArrayMake(arrayFromJson);
+      createHtmlList(restoArray);
     });
   }
-  dataHandler(console.table(arrayFromJson));
 }
 
 document.addEventListener('DOMContentLoaded', async () => mainEvent());
