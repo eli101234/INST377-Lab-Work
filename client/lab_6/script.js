7a44f79b4075664783ae73bf7db9323a6b4c1853
Eli Hernandez <elih717@gmail.com>
2022-03-12 20:09:50 -0500

    MAJOR OVERHALL
    I realized I did a lot of things wrong, ended up changing it completely

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,22 +1,35 @@
-// As the last step of your lab, hook this up to index.html
-def dataHandler(data);
-async function mainEvent() { // the async keyword means we can make API requests
-  let mainForm = document.querySelector('.right-box') ;
-  let button = document.querySelector('.button');
-  const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); 
-  const arrayFromJson = await results.json();
-  button.addEventListener("submit", e => {
-    button.style.disply = 'none';
-  });
-  const form = document.querySelector('.left-box');
-  if(0 > length.results) {
-  form.addEventListener('submit', async (submitEvent) => { 
-    submitEvent.preventDefault(); 
-    console.log('form submission'); 
-    console.table(arrayFromJson);
-    button.style.disply = 'none';
-  }); dataHandler();}
+function getRandomIntInclusive(min, max) {
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(
+    Math.random() * (newMax - newMin + 1) + newMin
+  );
 }
-  
-// this actually runs first! It's calling the function above
-document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+
+function dataHandler(dataArray) {
+  console.log('fired dataHandler');
+  console.table(dataArray);
+  const range = [...Array(15).keys()];
+  range.forEach((item) => {
+    console.log('rangeitem', item);
+  });
+}
+async function mainEvent() {
+  console.log('script loaded');
+  const form = document.querySelector('.left-box');
+  const submit = document.querySelector('.submit-button');
+  submit.style.display = 'none';
+
+  const results = await fetch('/api/foodServicesPG');
+  const arrayFromJson = await results.json();
+  if (arrayFromJson.data.length > 0) {
+    form.addEventListener('submit', async (submitEvent) => {
+      submitEvent.preventDefault();
+      console.log('form submission');
+      button.style.disply = 'block';
+    });
+  }
+  dataHandler(console.table(arrayFromJson));
+}
+
+document.addEventListener('DOMContentLoaded', async () => mainEvent());
